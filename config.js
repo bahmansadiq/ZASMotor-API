17ef8b375414620352ebea624274cb9f355dbaeb
bahmansadiq <bahman.sadiq@gmail.com>
2017-02-03 00:33:01 -0800

    config file modified

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 module.exports = {
 
-    'secret': 'i0loveC@r$',
-    //'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
-   // 'database': 'mongodb://teamphun:password@jello.modulusmongo.net:27017/hypu8bYg'
-      'database': 'mongodb://zas:password@ds157078.mlab.com:57078/zasmotors',
+    'secret'   : 'i0loveC@r$',
+    'database' : 'mongodb://zas:password@ds157078.mlab.com:57078/zasmotors',
 
-      
+
     'jwtSession': {
         session: false
     }
